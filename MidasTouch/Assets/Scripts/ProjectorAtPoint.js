2263274f9c6cad0b21fbd8dbbaca6605712ed23d
Steven Klise <steven@stevenklise.com>
2012-03-03 23:44:04 -0500

    Spawn projector looking at average of contact points

--- MidasTouch/Assets/Scripts/ProjectorAtPoint.js
+++ MidasTouch/Assets/Scripts/ProjectorAtPoint.js
@@ -12,12 +12,30 @@ function Update () {
 }
 
 function OnCollisionEnter (collision : Collision) {
+  Debug.Log(collision.contacts.length);
+  
   // Debug-draw all contact points and normals
+  var contactSum : Vector3 = Vector3.zero;
+  var normalSum : Vector3 = Vector3.zero;
   for (var contact : ContactPoint in collision.contacts) {
     Debug.DrawRay(contact.point, contact.normal, Color.white);
+    contactSum = contactSum + contact.point;
+    normalSum = normalSum + contact.normal;
   }
+  
+  var contactAverage = contactSum * (1.0 / collision.contacts.length);
+  var normalAverage = normalSum * (1.0 / collision.contacts.length);
+
+  // Make an empty gameObject to hold the Projectpr
+  var projectorWrapper = new GameObject();
+  // Set projectorWrapper as a child of this object
+  projectorWrapper.transform.parent = transform;
+  // And give it the same position as this object
+  projectorWrapper.transform.position = contactAverage + normalAverage * -1.5;
+  // Create a projector and attach it to the projectorWrapper
   var proj : Projector;
-  proj = gameObject.AddComponent ("Projector");
+  proj = projectorWrapper.AddComponent ("Projector");
+  projectorWrapper.transform.LookAt(contactAverage);
 }
 
 function OnCollisionStay (collision : Collision) {
